992901bf26f2f393dcc3cd4a10e5dde5a5cdb6b7
thejamilah <jamilahmcw@gmail.com>
2019-07-01 14:49:26 -0700

    module class

--- 05-object-inheritance/lib/phone.rb
+++ 05-object-inheritance/lib/phone.rb
@@ -1,2 +1,18 @@
-class Phone
+
+module Phone
+
+  module InstanceMethods
+
+    def make_call
+      puts "HEEEEYYYY"
+      puts "Pick up grams!"
+    end
+  end
+
+  module ClassMethods
+    def who_am_i
+      puts self
+    end
+  end
+
 end
